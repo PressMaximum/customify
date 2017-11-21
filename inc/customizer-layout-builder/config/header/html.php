14bc08e53601f2853c7b4cc868d271c9cdb9a8f8
TruongSa <shrimp2t@gmail.com>
2017-11-21 09:54:47 +0700

    Front-end: Add HTML item

--- inc/customizer-layout-builder/config/header/html.php
+++ inc/customizer-layout-builder/config/header/html.php
@@ -1,5 +1,6 @@
 <?php
 function customify_builder_config_header_html(){
+    $fn = 'customify_builder_header_html_item';
     return array(
         array(
             'name' => 'header_html',
@@ -13,9 +14,18 @@ function customify_builder_config_header_html(){
             'name' => 'header_html',
             'type' => 'textarea',
             'section' => 'header_html',
+            'selector' => '.builder-header-html-item',
+            'render_callback' => $fn,
             'theme_supports' => '',
             'title'          => __( 'HTML', 'customify' ),
             'description'          => __( 'Arbitrary HTML code.', 'customify' ),
         ),
     );
+}
+
+function customify_builder_header_html_item(){
+    $content = Customify_Customizer()->get_setting( 'header_html' );
+    echo '<div class="builder-header-html-item">';
+        echo apply_filters('customify_the_content', wp_kses_post( $content ) );
+    echo '</div>';
 }
