ebfbe4ea9350a26e83fc493de3d0cd15aaae59dc
TruongSa <shrimp2t@gmail.com>
2018-01-05 16:47:24 +0700

    Update template content

--- archive.php
+++ archive.php
@@ -29,12 +29,7 @@ get_header(); ?>
                     /* Start the Loop */
                     while ( have_posts() ) : the_post();
 
-                        /*
-                         * Include the Post-Format-specific template for the content.
-                         * If you want to override this in a child theme, then include a file
-                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                         */
-                        get_template_part( 'template-parts/content', get_post_format() );
+                        customify_the_blog_item();
 
                     endwhile;
 
