36eb4a70fa9089964e9a10e3a6be7f2f13d2dcda
TruongSa <shrimp2t@gmail.com>
2018-02-01 10:15:45 +0700

    Update menu styling

--- inc/customizer-layout-builder/config/header/menus.php
+++ inc/customizer-layout-builder/config/header/menus.php
@@ -97,7 +97,7 @@ class Customify_Builder_Item_Primary_Menu {
                 'selector'  => array(
                     'normal' => "{$this->selector} > li > a",
                     'normal_margin' => "{$this->selector} > li",
-                    'hover' => "{$this->selector} > li > a:hover, {$this->selector} > li.current-menu-item > a",
+                    'hover' => "{$this->selector} > li > a:hover, {$this->selector} > li.current-menu-item > a, {$this->selector} > li.current-menu-ancestor > a, {$this->selector} > li.current-menu-parent > a",
                     'hover_text_color' => "{$this->selector} > li > a:hover, {$this->selector} > li.current-menu-item > a, {$this->selector} > li.current-menu-ancestor > a, {$this->selector} > li.current-menu-parent > a",
                 ),
                 'css_format'  => 'styling',
