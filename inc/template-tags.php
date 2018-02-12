42d38354f9ef1b0c68b7886fdb8d65806e42b144
TruongSa <shrimp2t@gmail.com>
2018-02-12 11:26:29 +0700

    Update single post

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -49,17 +49,17 @@ if ( ! function_exists( 'customify_entry_footer' ) ) :
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'customify' ) );
+			$categories_list = get_the_category_list( ' ' );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'customify' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+				printf( '<span class="cat-links">' . __( '<span class="cat-label">Posted in</span> %1$s', 'customify' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'customify' ) );
+			$tags_list = get_the_tag_list( '', ' ' );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'customify' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				printf( '<span class="tags-links">' . __( '<span class="tagged-label">Tagged</span> %1$s', 'customify' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 
