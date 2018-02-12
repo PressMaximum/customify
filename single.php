e3b1281d649d977894fefc34b20dbfdb49f66a11
TruongSa <shrimp2t@gmail.com>
2018-02-12 11:40:27 +0700

    Update single css

--- single.php
+++ single.php
@@ -20,7 +20,10 @@ get_header(); ?>
 
                         get_template_part( 'template-parts/content', get_post_type() );
 
-                        the_post_navigation();
+                        the_post_navigation( array(
+                            'prev_text' => __( '<span>Prev post</span> %title', 'customify' ),
+                            'next_text' => __( '<span>Next post</span> %title', 'customify' ),
+                        ) );
 
                         // If comments are open or we have at least one comment, load up the comment template.
                         if ( comments_open() || get_comments_number() ) :
