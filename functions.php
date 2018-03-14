937a2a6d0616e62be6ad5180cdda14d2ce4e43f9
TruongSa <shrimp2t@gmail.com>
2018-03-14 09:32:39 +0700

    Remove Secondary menu, fixed #38

--- functions.php
+++ functions.php
@@ -130,8 +130,7 @@ class Customify_Init {
 
         // This theme uses wp_nav_menu() in one location.
         register_nav_menus( array(
-            'menu-1' => esc_html__( 'Primary', 'customify' ),
-            'menu-2' => esc_html__( 'Secondary', 'customify' ),
+            'menu-1' => esc_html__( 'Primary', 'customify' )
         ) );
 
         /*
