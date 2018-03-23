292b7c5dc5185597115a34061f38e79ce6addf53
TruongSa <shrimp2t@gmail.com>
2018-03-23 14:05:29 +0700

    test gruntfile i18n

--- inc/compatibility/breadcrumb-navxt.php
+++ inc/compatibility/breadcrumb-navxt.php
@@ -87,11 +87,11 @@ class Customify_Breadcrumb {
                 'type' => 'select',
                 'section' =>  $section,
                 'default' => 1,
-                'title' => __( 'Display Position', 'customify-pro' ),
+                'title' => __( 'Display Position', 'customify-pro', 'customify' ),
                 'choices' => apply_filters( 'customify/breadcrumb/config/positions', array(
-                    'below_header' => __( 'Display below header', 'customify-pro' ),
-                    'below_titlebar' => __( 'Display below titlebar', 'customify-pro' ),
-                    'inside_titlebar' => __( 'Display inside titlebar', 'customify-pro' ),
+                    'below_header' => __( 'Display below header', 'customify-pro', 'customify' ),
+                    'below_titlebar' => __( 'Display below titlebar', 'customify-pro', 'customify' ),
+                    'inside_titlebar' => __( 'Display inside titlebar', 'customify-pro', 'customify' ),
                 ) ),
             );
 
