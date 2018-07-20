6e3a7aa990600a0443cf06a95d438b623fc5ffe2
TruongSa <shrimp2t@gmail.com>
2018-07-20 17:52:28 +0700

    Get hook product title back #138

--- inc/compatibility/woocommerce/inc/template-hooks.php
+++ inc/compatibility/woocommerce/inc/template-hooks.php
@@ -8,9 +8,7 @@ function customify_wc_remove_default_hook (){
 
 	remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 	remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
-
-	remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
-
+	
 	remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 	remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 
