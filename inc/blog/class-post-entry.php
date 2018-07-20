c75d68a3fd4d5397b1938034404067020bfcb0fb
TruongSa <shrimp2t@gmail.com>
2018-07-20 14:49:09 +0700

    Add biography for single post, fixed #131

--- inc/blog/class-post-entry.php
+++ inc/blog/class-post-entry.php
@@ -509,6 +509,57 @@ class Customify_Post_Entry {
         }
     }
 
+
+    function post_author_bio()
+    {
+        if (is_single()) {
+            global $post;
+            // Detect if it is a single post with a post author
+            if (is_single() && isset($post->post_author)) {
+
+                $user = get_user_by('ID', $post->post_author );
+                if ( ! $user ) {
+                    return ;
+                }
+
+                $display_name = $user->display_name ? $user->display_name : $user->user_login;
+                // Get author's biographical information or description
+                $user_description = get_the_author_meta('user_description', $user->ID );
+                // Get author's website URL
+                $user_website = get_the_author_meta('url',  $user->ID );
+
+                // Get link to the author archive page
+                $user_posts = get_author_posts_url(get_the_author_meta('ID',  $user->ID ));
+
+                if (!empty($display_name)) {
+                    $author_details = '<h3 class="bio-author-heading">' . sprintf( __( 'About the Author: <span>%s</span>', 'customify' ), $display_name ) . '</h3>';
+                }
+
+                $user_description = wptexturize( $user_description );
+                $user_description = wpautop( $user_description );
+                $user_description = convert_smilies( $user_description );
+
+                $author_links = '<p class="author_links"><a href="' . $user_posts . '">' .sprintf( 'View all post by %s', $display_name ). '</a>';
+
+                // Check if author has a website in their profile
+                if (!empty($user_website)) {
+                    // Display author website link
+                    $author_links .= ' | <a href="' . $user_website . '" target="_blank" rel="nofollow">Website</a></p>';
+                } else {
+                    // if there is no author website then just close the paragraph
+                    $author_links .= '</p>';
+                }
+
+                $author_details .= '<div class="bio-author"><div class="bio-avatar">' . get_avatar(get_the_author_meta('user_email'), 90) . '</div><div class="bio-author-details">'. $user_description . $author_links.'</div></div>';
+
+                // Pass all this info to post content
+                $content =  '<div class="author-bio-section" >' . $author_details . '</div>';
+            }
+
+            echo $content;
+        }
+    }
+
     function post_navigation(){
         if ( ! is_single() ) {
             return '';
