e1d0f8ddbfd9b8f03dbd791f34da8c335c585f0b
Kien Trong <kienpc81089@gmail.com>
2018-09-09 18:02:38 +0700

    Update to version 0.2.2

--- inc/panel-builder/class-customize-layout-builder.php
+++ inc/panel-builder/class-customize-layout-builder.php
@@ -435,7 +435,7 @@ class Customify_Customize_Layout_Builder {
 
         <?php if ( ! apply_filters( 'customify/is_pro_activated', false )  ) { ?>
             <script type="text/html" id="customify-upsell-tmpl">
-                <p class="customify-upsell-panel"><?php _e( 'Enjoy building? Upgrade to <a target="_blank" href="https://wpcustomify.com/pricing/?utm_source=theme_dashboard&utm_medium=links&utm_campaign=panel_text">Customify Pro</a> to get more builder items</a>.', 'customify' ); ?></p>
+                <p class="customify-upsell-panel"><?php _e( 'Enjoy building? Upgrade to <a target="_blank" href="https://wpcustomify.com/pricing/?utm_source=theme_dashboard&utm_medium=links&utm_campaign=panel_text">Customify Pro</a> to get more builder items and other premium features</a>.', 'customify' ); ?></p>
             </script>
 			<?php
 		}
