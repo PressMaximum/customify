b8960c25ac9fbf243ec958060d97aa68cfe4ee8b
TruongSa <shrimp2t@gmail.com>
2018-09-14 09:20:32 +0700

    Improve gallery styling

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -135,4 +135,19 @@ if ( ! function_exists( 'customify_comment' ) ) :
 	}
 endif;
 
+if ( ! function_exists( 'customify_nav_next_icon' ) ) {
+	function customify_nav_next_icon(){
+		return '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129"><g><path d="m40.4,121.3c-0.8,0.8-1.8,1.2-2.9,1.2s-2.1-0.4-2.9-1.2c-1.6-1.6-1.6-4.2 0-5.8l51-51-51-51c-1.6-1.6-1.6-4.2 0-5.8 1.6-1.6 4.2-1.6 5.8,0l53.9,53.9c1.6,1.6 1.6,4.2 0,5.8l-53.9,53.9z"/></g></svg>';
+	}
+}
 
+add_filter( 'customify_nav_next_icon', 'customify_nav_next_icon' );
+
+
+if ( ! function_exists( 'customify_nav_prev_icon' ) ) {
+	function customify_nav_prev_icon(){
+		return '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129"><g><path d="m88.6,121.3c0.8,0.8 1.8,1.2 2.9,1.2s2.1-0.4 2.9-1.2c1.6-1.6 1.6-4.2 0-5.8l-51-51 51-51c1.6-1.6 1.6-4.2 0-5.8s-4.2-1.6-5.8,0l-54,53.9c-1.6,1.6-1.6,4.2 0,5.8l54,53.9z"/></g></svg>';
+	}
+}
+
+add_filter( 'customify_nav_prev_icon', 'customify_nav_prev_icon' );
