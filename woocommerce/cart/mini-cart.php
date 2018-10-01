c25ae58f1ef586917d951a92f13077dc65f6df82
TruongSa <shrimp2t@gmail.com>
2018-10-01 17:06:42 +0700

    fixed #190

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -26,28 +26,48 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 				$thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 				$product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 				$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+
+				if ( $product_name ) {
+					$product_name = '<span class="mini_cart_item__title">'.$product_name.'</span>';
+                }
+
 				?>
                 <li class="woocommerce-mini-cart-item <?php echo esc_attr( apply_filters( 'woocommerce_mini_cart_item_class', 'mini_cart_item', $cart_item, $cart_item_key ) ); ?>">
 					<?php
-					echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
-						'<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
-						esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-						__( 'Remove this item', 'customify' ),
-						esc_attr( $product_id ),
-						esc_attr( $cart_item_key ),
-						esc_attr( $_product->get_sku() )
-					), $cart_item_key );
+					if ( $thumbnail ) {
+						if ( ! empty( $product_permalink ) ) {
+                            echo '<a class="mini_cart_item__thumb" href="'.esc_url( $product_permalink ).'">';
+                                echo $thumbnail; // WPCS: XSS OK.
+                            echo '</a>';
+                        } else {
+							echo '<span class="mini_cart_item__thumb">';
+							echo $thumbnail; // WPCS: XSS OK.
+							echo '</span>';
+                        }
+                    }
 					?>
-					<?php if ( empty( $product_permalink ) ) : ?>
-						<?php echo $thumbnail . $product_name . '&nbsp;'; ?>
+                    <span class="mini_cart_item__info"><?php
+                        if ( empty( $product_permalink ) ) : ?>
+						<?php echo  $product_name; // WPCS: XSS OK. ?>
 					<?php else : ?>
                         <a href="<?php echo esc_url( $product_permalink ); ?>">
-							<?php echo $thumbnail . $product_name . '&nbsp;'; ?>
+							<?php echo  $product_name; // // WPCS: XSS OK.  ?>
                         </a>
 					<?php endif; ?>
 					<?php echo wc_get_formatted_cart_item_data( $cart_item ); ?>
-
 					<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity text-xsmall">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); ?>
+                    </span><?php
+
+                    echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
+	                    '<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
+	                    esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+	                    __( 'Remove this item', 'customify' ),
+	                    esc_attr( $product_id ),
+	                    esc_attr( $cart_item_key ),
+	                    esc_attr( $_product->get_sku() )
+                    ), $cart_item_key );
+
+                    ?>
                 </li>
 				<?php
 			}
