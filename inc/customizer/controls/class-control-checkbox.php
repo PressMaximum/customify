cf5ab5adcb51433693607b8e48bc93362c19a052
TruongSa <shrimp2t@gmail.com>
2018-10-19 14:36:31 +0700

    Update checkbox control style

--- inc/customizer/controls/class-control-checkbox.php
+++ inc/customizer/controls/class-control-checkbox.php
@@ -9,7 +9,15 @@ class Customify_Customizer_Control_Checkbox extends Customify_Customizer_Control
         <?php echo self::field_header(); ?>
         <div class="customify-field-settings-inner">
             <label>
-                <input type="checkbox" class="customify-input" <# if ( field.value == 1 ){ #> checked="checked" <# } #> data-name="{{ field.name }}" value="1">
+
+                <span class="onoffswitch">
+                    <input type="checkbox" class="onoffswitch-checkbox customify-input" <# if ( field.value == 1 ){ #> checked="checked" <# } #> data-name="{{ field.name }}" value="1">
+                    <span class="onoffswitch-label">
+                        <span class="onoffswitch-inner"></span>
+                        <span class="onoffswitch-switch"></span>
+                    </span>
+                </span>
+
                 {{{ field.checkbox_label }}}
             </label>
         </div>
