654041758e0b01e42322ef78a62cdfd95cd0e436
TruongSa <shrimp2t@gmail.com>
2018-11-06 10:06:43 +0700

    Test phpcs

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -11,14 +11,16 @@
 
 <section class="no-results not-found">
 
-    <div class="page-content widget-area">
+	<div class="page-content widget-area">
 		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+			?>
 
-            <p><?php
+			<p>
+			<?php
 				printf(
 					wp_kses(
-					/* translators: 1: link to WP admin new post page. */
+						/* translators: 1: link to WP admin new post page. */
 						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'customify' ),
 						array(
 							'a' => array(
@@ -28,25 +30,28 @@
 					),
 					esc_url( admin_url( 'post-new.php' ) )
 				);
-				?></p>
+			?>
+				</p>
 
 		<?php elseif ( is_search() ) : ?>
 
-            <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'customify' ); ?></p>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'customify' ); ?></p>
 			<?php
 			echo '<div class="widget">';
 			get_search_form();
 			echo '</div>';
 
-		else : ?>
+		else :
+			?>
 
-            <p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'customify' ); ?></p>
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'customify' ); ?></p>
 			<?php
 
 			echo '<div class="widget">';
 			get_search_form();
 			echo '</div>';
 
-		endif; ?>
-    </div><!-- .page-content -->
+		endif;
+		?>
+	</div><!-- .page-content -->
 </section><!-- .no-results -->
