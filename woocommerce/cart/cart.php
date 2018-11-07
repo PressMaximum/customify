5059dba4b14c47b4451fb9299317c1662f5eb4eb
TruongSa <shrimp2t@gmail.com>
2018-11-07 09:30:46 +0700

    Improve wpcs

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -53,7 +53,9 @@ do_action( 'woocommerce_before_cart' ); ?>
 							__( 'Remove this item', 'customify' ),
 							esc_attr( $product_id ),
 							esc_attr( $_product->get_sku() )
-						), $cart_item_key );
+						),
+							$cart_item_key
+						);
 						?>
 					</td>
 
@@ -100,13 +102,17 @@ do_action( 'woocommerce_before_cart' ); ?>
 						if ( $_product->is_sold_individually() ) {
 							$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
 						} else {
-							$product_quantity = woocommerce_quantity_input( array(
-								'input_name'   => "cart[{$cart_item_key}][qty]",
-								'input_value'  => $cart_item['quantity'],
-								'max_value'    => $_product->get_max_purchase_quantity(),
-								'min_value'    => '0',
-								'product_name' => $_product->get_name(),
-							), $_product, false );
+							$product_quantity = woocommerce_quantity_input(
+								array(
+									'input_name'   => "cart[{$cart_item_key}][qty]",
+									'input_value'  => $cart_item['quantity'],
+									'max_value'    => $_product->get_max_purchase_quantity(),
+									'min_value'    => '0',
+									'product_name' => $_product->get_name(),
+								),
+								$_product,
+								false
+							);
 						}
 
 						echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
@@ -131,7 +137,9 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 				<?php if ( wc_coupons_enabled() ) { ?>
 					<div class="coupon">
-						<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'customify' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'customify' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'customify' ); ?>"><?php esc_attr_e( 'Apply coupon', 'customify' ); ?></button>
+						<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'customify' ); ?></label>
+						<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'customify' ); ?>" />
+						<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'customify' ); ?>"><?php esc_attr_e( 'Apply coupon', 'customify' ); ?></button>
 						<?php do_action( 'woocommerce_cart_coupon' ); ?>
 					</div>
 				<?php } ?>
