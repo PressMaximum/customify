263c12df671a9550bfb04d3230fa9acfd07beab3
TruongSa <shrimp2t@gmail.com>
2018-11-16 15:46:14 +0700

    Improve loading animation

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -25,7 +25,7 @@ if ( strpos( $args['class'], 'add_to_cart_button' ) === false ) {
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%1$s" data-quantity="%2$s" class="%3$s" %4$s>%5$s</a>',
+		'<a href="%1$s" data-quantity="%2$s" class="%3$s" %4$s><span class="button-label">%5$s</span></a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 		esc_attr( $args['class'] ),
