2061f474b116728d530f4e96272f72fdda9413c3
TruongTuyen <hieudieuluong@gmail.com>
2019-01-29 12:06:27 +0700

    Fixed #251

--- inc/customizer/configs/header/logo.php
+++ inc/customizer/configs/header/logo.php
@@ -104,7 +104,7 @@ class Customify_Builder_Item_Logo {
 		if ( $logo_image ) {
 			?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo-link" rel="home" itemprop="url">
-				<img class="site-img-logo" src="<?php echo esc_url( $logo_image ); ?>" alt="<?php esc_attr_e( 'Logo', 'customify' ); ?>"<?php if ( $logo_retina_image ) {
+				<img class="site-img-logo" src="<?php echo esc_url( $logo_image ); ?>" alt="<?php bloginfo( 'name' ); ?>"<?php if ( $logo_retina_image ) {
 					?> srcset="<?php echo esc_url( $logo_retina_image ); ?> 2x"<?php } ?>>
 				<?php do_action( 'customizer/after-logo-img' ); ?>
 			</a>
