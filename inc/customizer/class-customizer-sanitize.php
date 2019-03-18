c8b9ea663aaf84c5085e340b8c0ab251c3383040
TruongTuyen <hieudieuluong@gmail.com>
2019-03-18 17:41:24 +0700

    Update changelog, improve #260

--- inc/customizer/class-customizer-sanitize.php
+++ inc/customizer/class-customizer-sanitize.php
@@ -442,7 +442,7 @@ class Customify_Sanitize_Input {
 				$value = $this->sanitize_text_field_deep( $value );
 				break;
 			case 'textarea':
-				$value = wp_filter_nohtml_kses( $value );
+				$value = wp_filter_post_kses( $value );
 				break;
 			default:
 				$has_device = false;
