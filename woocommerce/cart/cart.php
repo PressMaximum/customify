464c76e94092848deca4b732b5733934c9dc8269
TruongTuyen <hieudieuluong@gmail.com>
2020-09-07 11:08:21 +0700

    Update woo template version

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -4,7 +4,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.8.0
+ * @version 4.4.0
  */
 
 defined( 'ABSPATH' ) || exit;
