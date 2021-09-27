c0e79abf1e55a1655e0a55dba2d38ac1e5b2a229
TruongTuyen <hieudieuluong@gmail.com>
2021-09-27 18:04:03 +0700

    Updated issue 177 in pro

--- inc/customizer/configs/background.php
+++ inc/customizer/configs/background.php
@@ -88,6 +88,32 @@ class Customify_Advanced_Styling_Background {
 				),
 			),
 
+			array(
+				'name'       => 'content_background',
+				'type'       => 'styling',
+				'section'    => 'background',
+				'title'      => __( 'Site Content Background', 'customify-pro' ),
+				'selector'   => array(
+					'normal'            => '.site-content',
+				),
+				'css_format' => 'styling', // styling.
+				'fields'     => array(
+					'normal_fields' => array(
+						'text_color' => false,
+						'link_color' => false,
+						'padding'     => false,
+						'margin'     => false,
+						'border_heading' => false,
+						'border_width' => false,
+						'border_color' => false,
+						'border_radius' => false,
+						'box_shadow' => false,
+						'border_style'  => false,
+					),
+					'hover_fields'  => false,
+				),
+			),
+
 		);
 
 		return array_merge( $configs, $config );
