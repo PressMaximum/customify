2b679e8880e0876c67fec4fa10714a9f1be8eb67
shrimp2t <shrimp2t@gmail.com>
2023-09-27 15:03:35 +0700

    Update WC templates

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -5,7 +5,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.5.2
+ * @version 7.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
