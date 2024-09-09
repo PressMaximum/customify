c4a5c31493fe2613728dabdf6c2bc5672cb94712
password <shrimp2t@gmail.com>
2024-09-09 08:55:59 +0700

    Fix WC template issues and update theme version

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -5,7 +5,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 7.8.0
+ * @version 9.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
