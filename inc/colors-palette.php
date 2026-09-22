37f5b0de08fd2c2ac7b198cf6eb94ccebf5743f8
TruongSa <shrimp2t@gmail.com>
2026-09-22 20:12:00 +0700

    fix(colors): include fallback in copied palette tokens

--- inc/colors-palette.php
+++ inc/colors-palette.php
@@ -1131,7 +1131,7 @@ if ( ! function_exists( 'customify_color_palette_quickpick_js' ) ) {
 		if ( slotKey ) {
 			var \$tokenRow = \$('<div class=\"customify-color-tokenrow\"></div>');
 			var \$token = \$('<input type=\"text\" class=\"customify-color-token\" readonly />')
-				.val('var(--customify-' + slotKey + ')');
+				.val('var(--customify-' + slotKey + ', ' + currentVal + ')');
 			\$token.on('focus', function(){ this.select(); });
 			\$token.on('click', function(e){ e.stopPropagation(); this.select(); });
 			\$tokenRow.append(\$token);
